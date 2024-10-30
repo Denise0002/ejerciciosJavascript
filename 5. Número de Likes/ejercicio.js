71b4ebfd2b65f0edd2261926e7da37cc2a25f935
Denise0002 <hualasmarcos@gmail.com>
2024-10-30 12:46:16 -0500

    feat:solucion del ejercicio

---
+++ 5. Número de Likes/ejercicio.js
@@ -0,0 +1,15 @@
+// escribe tu respuesta acá
+function likes(numero) {
+    if (numero < 1000) {
+        return numero + '';
+    } else if (numero < 1_000_000) {
+        return (numero / 1000 | 0) + 'K'; 
+    } else {
+        return (numero / 1_000_000 | 0) + 'M'; 
+}
+// código de prueba
+console.log(likes(983)) // "983"
+console.log(likes(1900)) // "1K"
+console.log(likes(54000)) // "54K"
+console.log(likes(120800)) // "120K"
+console.log(likes(25222444)) // "25M"
